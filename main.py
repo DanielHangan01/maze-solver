16821865ff5cb0332aa9b72dce8c5d942cc4a224
DanielHangan01 <danhangan@hotmail.com>
2023-09-03 02:57:43 +0200

    drawing lines now possible

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    line1 = Line(Point(0, 0), Point(100, 200))
+    line2 = Line(Point(300, 300), Point(500, 500))
+    win.draw_line(line1, "red")
+    win.draw_line(line2)
     win.wait_for_close()
 
 main()
