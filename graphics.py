f87bb8cc5664e5ba0bf386be15638e485bf8b013
DanielHangan01 <danhangan@hotmail.com>
2023-09-03 02:46:42 +0200

    Root Widget and Canvas now customized for dimension, main in separate file, all attributes private

--- graphics.py
+++ graphics.py
@@ -3,26 +3,22 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
         self.__root = Tk()
-        self.__root.title("Maze")
+        self.__root.title("Maze Solver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.running = False
+        self.__canvas = Canvas(self.__root, bg = "white", height = height, width = width)
+        self.__canvas.pack(fill = BOTH, expand = 1)
+        self.__running = False
 
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
+        print("Window closed")
     
     def close(self):
-        self.running = False
-
-win = Window(800, 600)
-win.wait_for_close()
+        self.__running = False
