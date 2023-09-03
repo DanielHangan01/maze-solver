0c9fc0d25bafb90468c902236689ec963f94f049
DanielHangan01 <danhangan@hotmail.com>
2023-09-03 03:41:28 +0200

    move between two cells

--- cell.py
+++ cell.py
@@ -30,4 +30,51 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    
+    """
+    function takes into account that the cell may be larger than one individual unit
+    hence depending on the direction, it draws two separate lines:
+    one between a corner point of the origin point to its center in that direction
+    and another one between that center and the destination cell center in that direction
+    therefore it is guaranteed that the two relative point centers are connected
+    """
+    def draw_move(self, to_cell, undo = False):
+        x_mid = (self._x1 + self._x2) / 2
+        y_mid = (self._y1 + self._y2) / 2
+
+        to_x_mid = (to_cell._x1 + to_cell._x2) / 2
+        to_y_mid = (to_cell._y1 + to_cell._y2) / 2
+        
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+        
+        # left direction
+        if self._x1 > to_cell._x1:
+            line = Line(Point(self._x1, y_mid), Point(x_mid, y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_cell._x2, to_y_mid))
+            self._win.draw_line(line, fill_color)
+
+        # right direction
+        elif self._x1 < to_cell._x1:
+            line = Line(Point(x_mid, y_mid), Point(self._x2, y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_cell._x1, to_y_mid), Point(to_x_mid, to_y_mid))
+            self._win.draw_line(line, fill_color)
+
+        # upwards direction
+        elif self._y1 > to_cell._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y1))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_cell._y2), Point(to_x_mid, to_y_mid))
+            self._win.draw_line(line, fill_color)
+
+        # downwards direction
+        elif self._y1 < to_cell._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y2))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_x_mid, to_cell._y1))
+            self._win.draw_line(line, fill_color)
     
