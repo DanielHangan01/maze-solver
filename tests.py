6c51e8bf1c7b50e8f2aadc24dae7b5b3bdfa3eae
DanielHangan01 <danhangan@hotmail.com>
2023-09-05 00:06:58 +0200

    changed x,y(1,2) order and added coordinates test for maze cells

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 from maze import Maze
+from graphics import Window
 
 class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
@@ -8,6 +9,17 @@ class Tests(unittest.TestCase):
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(len(m1._cells), num_cols)
         self.assertEqual(len(m1._cells[0]), num_rows)
+
+    def test_maze_coordinates(self):
+        num_cols = 10
+        num_rows = 10
+        m1 = Maze(200, 200, num_rows, num_cols, 10, 20, win = Window(800, 600))
+        self.assertEqual(len(m1._cells), num_cols)
+        self.assertEqual(len(m1._cells[0]), num_rows)
+        self.assertEqual(m1._cells[5][3]._x1, 210)
+        self.assertEqual(m1._cells[5][3]._x2, 250)
+        self.assertEqual(m1._cells[8][9]._y1, 220)
+        self.assertEqual(m1._cells[8][9]._y2, 380)
 
 if __name__ == "__main__":
     unittest.main()
