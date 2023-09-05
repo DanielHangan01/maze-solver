60470a644005d2292886b0c15420079e33dfeb41
DanielHangan01 <danhangan@hotmail.com>
2023-09-05 22:24:38 +0200

    entrance and exit

--- maze.py
+++ maze.py
@@ -12,22 +12,25 @@ class Maze:
         self._cell_size_y = cell_size_y
         self._win = win
         self._create_cells()
+        self._break_entrance_and_exit()
 
     def _create_cells(self):
         for i in range(self._num_cols):
-            self._cells.append([])
-            self._cells[i] = []
+            col_cells = []
             for j in range(self._num_rows):
-                self._cells[i].append(Cell(self._win))
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
         if self._win is None:
             return
-        x1 = self._x1 +  self._cell_size_x
-        y1 = self._y1 + self._cell_size_y
-        x2 = self._x1 + i * self._cell_size_x
-        y2 = self._y1 + j * self._cell_size_y
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
 
         self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
@@ -36,6 +39,14 @@ class Maze:
         if self._win is None:
             return
         self._win.redraw()
-        time.sleep(0.01)
+        time.sleep(0.005)
+
+    def _break_entrance_and_exit(self):
+        entrance = self._cells[0][0]
+        exit = self._cells[self._num_cols - 1][self._num_rows - 1]
+        entrance.has_top_wall = False
+        self._draw_cell(0, 0)
+        exit.has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
                 
             
