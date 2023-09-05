1d94f3d4a73d705a8e53e42fd60d49c0e9d94728
DanielHangan01 <danhangan@hotmail.com>
2023-09-05 22:58:17 +0200

    reset revisited + unit test, maze coordinates unit test fixed

--- maze.py
+++ maze.py
@@ -19,6 +19,7 @@ class Maze:
         self._create_cells()
         self._break_entrance_and_exit()
         self._break_walls_r(0, 0)
+        self._reset_cells_visited()
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -91,5 +92,8 @@ class Maze:
             #visit next cell
             self._break_walls_r(next[0], next[1])
                         
-                
+    def _reset_cells_visited(self):
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._cells[i][j].visited = False
             
--- tests.py
+++ tests.py
@@ -16,12 +16,10 @@ class Tests(unittest.TestCase):
         m1 = Maze(200, 200, num_rows, num_cols, 10, 20, win = Window(800, 600))
         self.assertEqual(len(m1._cells), num_cols)
         self.assertEqual(len(m1._cells[0]), num_rows)
-        #self.assertEqual(m1._cells[5][3]._x1, 210)
-        #self.assertEqual(m1._cells[5][3]._x2, 250)
-        #self.assertEqual(m1._cells[8][9]._y1, 220)
-        #self.assertEqual(m1._cells[8][9]._y2, 380)
-
-        m1._win.wait_and_close()
+        self.assertEqual(m1._cells[5][3]._x1, 250)
+        self.assertEqual(m1._cells[5][3]._x2, 260)
+        self.assertEqual(m1._cells[8][9]._y1, 380)
+        self.assertEqual(m1._cells[8][9]._y2, 400)
 
     def test_maze_break_entrance_and_exit(self):
         num_cols = 12
@@ -29,8 +27,14 @@ class Tests(unittest.TestCase):
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertFalse(m1._cells[0][0].has_top_wall)
         self.assertFalse(m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall)
-
-        m1._win.wait_and_close()
+    
+    def test_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, Window(800, 600), 10)
+        for i in range(num_cols):
+            for j in range(num_rows):
+                self.assertFalse(m1._cells[i][j].visited)
 
 if __name__ == "__main__":
     unittest.main()
