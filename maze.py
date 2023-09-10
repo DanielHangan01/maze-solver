cbd3cd62dbbd392ba6d4ef677959b09fdbef1c51
DanielHangan01 <danhangan@hotmail.com>
2023-09-10 20:51:53 +0200

    Entrance can now be set at any point of any edge

--- maze.py
+++ maze.py
@@ -3,7 +3,7 @@ import time
 import random
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win = None, seed = None):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win = None, seed = None, start_x = 0,start_y = 0):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
@@ -12,13 +12,15 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+        self._start_x = start_x
+        self._start_y = start_y
 
         if seed:
             random.seed(seed)
         
         self._create_cells()
         self._break_entrance_and_exit()
-        self._break_walls_r(0, 0)
+        self._break_walls_r(start_x, start_y)
         self._reset_cells_visited()
 
     def _create_cells(self):
@@ -49,10 +51,19 @@ class Maze:
         time.sleep(0.005)
 
     def _break_entrance_and_exit(self):
-        entrance = self._cells[0][0]
+        if self._start_x != 0 and self._start_x != self._num_cols - 1 and self._start_y != 0 and self._start_y != self._num_rows - 1:
+            raise Exception("Entrance is supposed to be on one of the edges")
+        entrance = self._cells[self._start_x][self._start_y]
         exit = self._cells[self._num_cols - 1][self._num_rows - 1]
-        entrance.has_top_wall = False
-        self._draw_cell(0, 0)
+        if self._start_x == 0 and self._start_y == 0:
+            entrance.has_top_wall = False
+        elif self._start_x == 0:
+            entrance.has_left_wall = False
+        elif self._start_y == 0:
+            entrance.has_right_wall = False
+        else:
+            entrance.has_bottom_wall = False
+        self._draw_cell(self._start_x, self._start_y)
         exit.has_bottom_wall = False
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
 
@@ -98,7 +109,7 @@ class Maze:
                 self._cells[i][j].visited = False
 
     def solve(self):
-        return self._solve_r(0, 0)
+        return self._solve_r(self._start_x, self._start_y)
 
     def _solve_r(self, i, j):
         self._animate()
